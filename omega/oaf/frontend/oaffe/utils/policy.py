52ef96cddd53dac40f1e06872fce707d0edf7dd0
Michael Scovetta <michael.scovetta@gmail.com>
2023-05-25 23:01:54 -0700

    Add three minute timeout to policy evaluation command.
    Signed-off-by: Michael Scovetta <michael.scovetta@gmail.com>

--- omega/oaf/frontend/oaffe/utils/policy.py
+++ omega/oaf/frontend/oaffe/utils/policy.py
@@ -31,62 +31,70 @@ def refresh_policies(subject: Subject = None, clear_first: bool = False):
                 f.write(json.dumps(assertion.content, indent=2))
 
         # Run the policy execution tool (out of process)
-        res = subprocess.run(
-            [
-                sys.executable,
-                "oaf.py",
-                "--verbose",
-                "consume",
-                "--repository",
-                f"flatdir:{tmpdir}",
-                "--subject",
-                subject.identifier,
-            ],
-            cwd=os.path.join(pathlib.Path().resolve(), "../../../omega/oaf/omega"),
-            capture_output=True,
-            encoding="utf-8",
-        )
+        res = None
+        try:
+            res = subprocess.run(
+                [
+                    sys.executable,
+                    "oaf.py",
+                    "--verbose",
+                    "consume",
+                    "--repository",
+                    f"flatdir:{tmpdir}",
+                    "--subject",
+                    subject.identifier,
+                ],
+                cwd=os.path.join(pathlib.Path().resolve(), "../../../omega/oaf/omega"),
+                capture_output=True,
+                timeout=180,
+                encoding="utf-8",
+            )
 
-        if res.returncode == 0:
-            try:
-                results = json.loads(res.stdout)
-            except Exception as msg:
-                logging.warning("Error parsing oaf output: %s", msg)
-                return
+            if res.returncode == 0:
+                try:
+                    results = json.loads(res.stdout)
+                except Exception as msg:
+                    logging.warning("Error parsing oaf output: %s", msg)
+                    return
 
-            already_cleared = False
+                already_cleared = False
 
-            for result in results:
-                policy_identifier = result.get("policy_identifier")
-                policy_name = result.get("policy_name")
+                for result in results:
+                    policy_identifier = result.get("policy_identifier")
+                    policy_name = result.get("policy_name")
 
-                policy, _ = Policy.objects.get_or_create(
-                    identifier=policy_identifier, defaults={"name": policy_name}
-                )
+                    policy, _ = Policy.objects.get_or_create(
+                        identifier=policy_identifier, defaults={"name": policy_name}
+                    )
 
-                _status = result.get("state", "").lower().strip()
-                if _status == "pass":
-                    status = PolicyEvaluationResult.Status.PASSED
-                elif _status in ["fail", "failed"]:
-                    status = PolicyEvaluationResult.Status.FAILED
-                elif _status != "":
-                    status = PolicyEvaluationResult.Status.INDETERMINATE
-                else:
-                    status = PolicyEvaluationResult.Status.UNKNOWN
+                    _status = result.get("state", "").lower().strip()
+                    if _status == "pass":
+                        status = PolicyEvaluationResult.Status.PASSED
+                    elif _status in ["fail", "failed"]:
+                        status = PolicyEvaluationResult.Status.FAILED
+                    elif _status != "":
+                        status = PolicyEvaluationResult.Status.INDETERMINATE
+                    else:
+                        status = PolicyEvaluationResult.Status.UNKNOWN
 
-                evaluated_by = "org.openssf.alpha-omega.oaf"
+                    evaluated_by = "org.openssf.alpha-omega.oaf"
 
-                logging.debug("Policy %s for %s is %s", policy, subject, status)
+                    logging.debug("Policy %s for %s is %s", policy, subject, status)
 
 
-                if clear_first and not already_cleared:
-                    already_cleared = True
-                    PolicyEvaluationResult.objects.filter(subject=subject).delete()
+                    if clear_first and not already_cleared:
+                        already_cleared = True
+                        PolicyEvaluationResult.objects.filter(subject=subject).delete()
 
-                PolicyEvaluationResult.objects.update_or_create(
-                    policy=policy, subject=subject, status=status, evaluated_by=evaluated_by
-                )
-        else:
-            logging.warning("Error evaluating assertion, return code: %d", res.returncode)
+                    PolicyEvaluationResult.objects.update_or_create(
+                        policy=policy, subject=subject, status=status, evaluated_by=evaluated_by
+                    )
+            else:
+                logging.warning("Error evaluating assertion, return code: %d", res.returncode)
+                logging.warning("STDOUT: %s", res.stdout)
+                logging.warning("STDERR: %s", res.stderr)
+
+        except subprocess.TimeoutExpired:
+            logging.warning("Timeout evaluating assertion")
             logging.warning("STDOUT: %s", res.stdout)
             logging.warning("STDERR: %s", res.stderr)
